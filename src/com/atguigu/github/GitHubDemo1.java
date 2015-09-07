992e2bffaba2ab5679c70934199268c673248a57
zzyybs <zzyybs@126.com>
2015-09-07 16:08:45 +0800

    Update GitHubDemo1.java
    github response 01

--- src/com/atguigu/github/GitHubDemo1.java
+++ src/com/atguigu/github/GitHubDemo1.java
@@ -5,5 +5,6 @@ public class GitHubDemo1
 	public static void main(String[] args)
 	{
 		System.out.println("welcome to atguigu,0423，同学们好");
+		System.out.println("Github send message to 0423,服务器收到");
 	}
 }
